176b684d18f0e0a12a21663415bda3102a0b3f7e
jradekker <jradekker@gmail.com>
2017-06-24 22:13:22 +0200

    this my second assignment

---
+++ dating.js
@@ -0,0 +1,22 @@
+
+
+let username = document.getElementById('username');
+
+
+const names = ["Anne", "Jim", "Monique", "Hanneke", "Peter", "Mathilde", "Bastiaan", "Maurits", "Machteld"];
+names.length;
+names[names.length];
+names[names.length - 1];
+
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const RandomNumber = getRandomNumber(0, names.length); // => this const will contain the random number
+
+object.onclick = function showMyfriend()
+{
+ console.log(RandomNumber);
+console.log(username);
+}
